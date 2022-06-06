c7ee78451fd812c78013655ffee339f7155ea662
Ruben Paz <me@ruben.io>
2022-06-06 06:12:40 +0100

    chore: Align gpio with conventions

--- src/drivers/gpio.rs
+++ src/drivers/gpio.rs
@@ -1,8 +1,6 @@
 #![allow(dead_code)]
 
-/// GPIO Module as defined in the HiFive RevB board.
-/// TODO: Validate if this is a common standard
-use core::ptr::write_volatile;
+use core::ptr::{read_volatile, write_volatile};
 
 const GPIO_IN_VAL: u32 = 0x00 as u32;
 const GPIO_IN_EN: u32 = 0x04 as u32;
@@ -25,36 +23,163 @@ const GPIO_PASSTHRU_HIGH_IE: u32 = 0x44 as u32;
 const GPIO_PASSTHRU_LOW_IE: u32 = 0x48 as u32;
 
 pub struct GPIO {
-    base: *mut u32,
+    ptr: *mut u32,
 }
 
 impl GPIO {
-    pub fn new(base: *mut u32) -> Self {
-        GPIO { base }
+    pub fn new(ptr: *mut u32) -> Self {
+        GPIO { ptr }
     }
 
-    pub fn set_output_enabled(&mut self, pin: u32) {
-        unsafe {
-            write_volatile((self.base as u32 + GPIO_OUT_EN) as *mut u32, pin);
-            write_volatile((self.base as u32 + GPIO_OUT_XOR) as *mut u32, pin);
-        }
+    pub fn input_val(&self) -> u32 {
+        unsafe { read_volatile((self.ptr as u32 + GPIO_IN_VAL) as *mut u32) }
     }
 
-    pub fn set_output_value(&mut self, value: u32) {
-        unsafe {
-            write_volatile((self.base as u32 + GPIO_OUT_VAL) as *mut u32, value);
-        }
+    pub fn set_input_val(&mut self, value: u32) {
+        unsafe { write_volatile((self.ptr as u32 + GPIO_IN_VAL) as *mut u32, value) }
     }
 
-    pub fn set_iof_enabled(&mut self, pin: u32) {
-        unsafe {
-            write_volatile((self.base as u32 + GPIO_IOF_EN) as *mut u32, pin);
-        }
+    pub fn input_en(&self) -> u32 {
+        unsafe { read_volatile((self.ptr as u32 + GPIO_IN_EN) as *mut u32) }
     }
 
-    pub fn set_iof_selection(&mut self, value: u32) {
-        unsafe {
-            write_volatile((self.base as u32 + GPIO_IOF_SEL) as *mut u32, value);
-        }
+    pub fn set_input_en(&mut self, value: u32) {
+        unsafe { write_volatile((self.ptr as u32 + GPIO_IN_EN) as *mut u32, value) }
+    }
+
+    pub fn output_en(&self) -> u32 {
+        unsafe { read_volatile((self.ptr as u32 + GPIO_OUT_EN) as *mut u32) }
+    }
+
+    pub fn set_output_en(&mut self, value: u32) {
+        unsafe { write_volatile((self.ptr as u32 + GPIO_OUT_EN) as *mut u32, value) }
+    }
+
+    pub fn output_val(&self) -> u32 {
+        unsafe { read_volatile((self.ptr as u32 + GPIO_OUT_VAL) as *mut u32) }
+    }
+
+    pub fn set_output_val(&mut self, value: u32) {
+        unsafe { write_volatile((self.ptr as u32 + GPIO_OUT_VAL) as *mut u32, value) }
+    }
+
+    pub fn pue(&self) -> u32 {
+        unsafe { read_volatile((self.ptr as u32 + GPIO_PUE) as *mut u32) }
+    }
+
+    pub fn set_pue(&mut self, value: u32) {
+        unsafe { write_volatile((self.ptr as u32 + GPIO_PUE) as *mut u32, value) }
+    }
+
+    pub fn ds(&self) -> u32 {
+        unsafe { read_volatile((self.ptr as u32 + GPIO_DS) as *mut u32) }
+    }
+
+    pub fn set_ds(&mut self, value: u32) {
+        unsafe { write_volatile((self.ptr as u32 + GPIO_DS) as *mut u32, value) }
+    }
+
+    pub fn rise_ie(&self) -> u32 {
+        unsafe { read_volatile((self.ptr as u32 + GPIO_RISE_IE) as *mut u32) }
+    }
+
+    pub fn set_rise_ie(&mut self, value: u32) {
+        unsafe { write_volatile((self.ptr as u32 + GPIO_RISE_IE) as *mut u32, value) }
+    }
+
+    pub fn rise_ip(&self) -> u32 {
+        unsafe { read_volatile((self.ptr as u32 + GPIO_RISE_IP) as *mut u32) }
+    }
+
+    pub fn set_rise_ip(&mut self, value: u32) {
+        unsafe { write_volatile((self.ptr as u32 + GPIO_RISE_IP) as *mut u32, value) }
+    }
+
+    pub fn fall_ie(&self) -> u32 {
+        unsafe { read_volatile((self.ptr as u32 + GPIO_FALL_IE) as *mut u32) }
+    }
+
+    pub fn set_fall_ie(&mut self, value: u32) {
+        unsafe { write_volatile((self.ptr as u32 + GPIO_FALL_IE) as *mut u32, value) }
+    }
+
+    pub fn fall_ip(&self) -> u32 {
+        unsafe { read_volatile((self.ptr as u32 + GPIO_FALL_IP) as *mut u32) }
+    }
+
+    pub fn set_fall_ip(&mut self, value: u32) {
+        unsafe { write_volatile((self.ptr as u32 + GPIO_FALL_IP) as *mut u32, value) }
+    }
+
+    pub fn high_ie(&self) -> u32 {
+        unsafe { read_volatile((self.ptr as u32 + GPIO_HIGH_IE) as *mut u32) }
+    }
+
+    pub fn set_high_ie(&mut self, value: u32) {
+        unsafe { write_volatile((self.ptr as u32 + GPIO_HIGH_IE) as *mut u32, value) }
+    }
+
+    pub fn high_ip(&self) -> u32 {
+        unsafe { read_volatile((self.ptr as u32 + GPIO_HIGH_IP) as *mut u32) }
+    }
+
+    pub fn set_high_ip(&mut self, value: u32) {
+        unsafe { write_volatile((self.ptr as u32 + GPIO_HIGH_IP) as *mut u32, value) }
+    }
+
+    pub fn low_ie(&self) -> u32 {
+        unsafe { read_volatile((self.ptr as u32 + GPIO_LOW_IE) as *mut u32) }
+    }
+
+    pub fn set_low_ie(&mut self, value: u32) {
+        unsafe { write_volatile((self.ptr as u32 + GPIO_LOW_IE) as *mut u32, value) }
+    }
+
+    pub fn low_ip(&self) -> u32 {
+        unsafe { read_volatile((self.ptr as u32 + GPIO_LOW_IP) as *mut u32) }
+    }
+
+    pub fn set_low_ip(&mut self, value: u32) {
+        unsafe { write_volatile((self.ptr as u32 + GPIO_LOW_IP) as *mut u32, value) }
+    }
+
+    pub fn iof_en(&self) -> u32 {
+        unsafe { read_volatile((self.ptr as u32 + GPIO_IOF_EN) as *mut u32) }
+    }
+
+    pub fn set_iof_en(&mut self, value: u32) {
+        unsafe { write_volatile((self.ptr as u32 + GPIO_IOF_EN) as *mut u32, value) }
+    }
+
+    pub fn iof_sel(&self) -> u32 {
+        unsafe { read_volatile((self.ptr as u32 + GPIO_IOF_SEL) as *mut u32) }
+    }
+
+    pub fn set_iof_sel(&mut self, value: u32) {
+        unsafe { write_volatile((self.ptr as u32 + GPIO_IOF_SEL) as *mut u32, value) }
+    }
+
+    pub fn out_xor(&self) -> u32 {
+        unsafe { read_volatile((self.ptr as u32 + GPIO_OUT_XOR) as *mut u32) }
+    }
+
+    pub fn set_out_xor(&mut self, value: u32) {
+        unsafe { write_volatile((self.ptr as u32 + GPIO_OUT_XOR) as *mut u32, value) }
+    }
+
+    pub fn passthru_high_ie(&self) -> u32 {
+        unsafe { read_volatile((self.ptr as u32 + GPIO_PASSTHRU_HIGH_IE) as *mut u32) }
+    }
+
+    pub fn set_passthru_high_ie(&mut self, value: u32) {
+        unsafe { write_volatile((self.ptr as u32 + GPIO_PASSTHRU_HIGH_IE) as *mut u32, value) }
+    }
+
+    pub fn passthru_low_ie(&self) -> u32 {
+        unsafe { read_volatile((self.ptr as u32 + GPIO_PASSTHRU_LOW_IE) as *mut u32) }
+    }
+
+    pub fn set_passthru_low_ie(&mut self, value: u32) {
+        unsafe { write_volatile((self.ptr as u32 + GPIO_PASSTHRU_LOW_IE) as *mut u32, value) }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -21,8 +21,9 @@ fn main_loop() {
 pub fn _start() {
     let mut gpio = GPIO::new(GPIO_ADDR);
     // Turn on the green led
-    gpio.set_output_enabled(LED_GREEN);
-    gpio.set_output_value(LED_GREEN);
+    gpio.set_output_en(LED_GREEN);
+    gpio.set_out_xor(LED_GREEN);
+    gpio.set_output_val(LED_GREEN);
 
     setup_clock();
     let uart = setup_uart0();
@@ -51,8 +52,8 @@ pub fn setup_clock() {
 pub fn setup_uart0() -> UART {
     // Enble UART GPIOs
     let mut gpio = GPIO::new(GPIO_ADDR);
-    gpio.set_iof_enabled(UART0_PIN_TX | UART0_PIN_RX);
-    gpio.set_iof_selection(0x0);
+    gpio.set_iof_en(UART0_PIN_RX | UART0_PIN_TX);
+    gpio.set_iof_sel(0x0);
 
     let mut uart = UART::new(UART0_ADDR);
     uart.txctrl.set_txen(true);
@@ -66,7 +67,7 @@ pub fn setup_uart0() -> UART {
 fn panic(_er: &PanicInfo) -> ! {
     let mut gpio = GPIO::new(GPIO_ADDR);
     // Turn on the red led
-    gpio.set_output_enabled(LED_RED);
-    gpio.set_output_value(LED_RED);
+    gpio.set_output_en(LED_RED);
+    gpio.set_output_val(LED_RED);
     loop {}
 }
